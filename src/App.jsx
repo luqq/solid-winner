c508d70c8351e8e31c870f49a1afd8b3c87e5c70
Jose C <luqq@users.noreply.github.com>
2025-03-10 06:15:24 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "José Cardós",
+  title: "DevOps and Software Engineer",
+  email: "luqquino@gmail.com",
+  gitHub: "luqq",
+  instagram: "luqq",
+  linkedIn: "JoseCardos",
   medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  twitter: "luqq",
+  youTube: "luqquino",
 };
 
 const primaryColor = "#4E567E";
