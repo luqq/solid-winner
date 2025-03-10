35e032dd421d698fcbe7ec6a3eca5f340a921d64
Jose C <luqq@users.noreply.github.com>
2025-03-10 06:52:43 +0000

    Final

---
+++ src/Components/Education.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <section className="padding" id="education">
+      <h2 style={{ textAlign: "center" }}>Education</h2>
+      <div className="container">
+        <div className="box">
+          <h3>University of Lagos</h3>
+          <p className="small">BSc. Computer Science</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Education;
